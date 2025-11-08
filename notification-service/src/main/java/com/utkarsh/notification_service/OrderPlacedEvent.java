dea03eccafd6895bb2cc83804c6d3fa4adceee49
Utkarsh <your-email@example.com>
2025-11-09 02:10:22 +0530

    Add Kafka configuration and integrate OrderPlacedEvent for notification service

---
+++ notification-service/src/main/java/com/utkarsh/notification_service/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.utkarsh.notification_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
---
+++ order-service/src/main/java/com/utkarsh/order_service/event/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.utkarsh.order_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
